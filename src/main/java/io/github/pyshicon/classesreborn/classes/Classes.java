42fe85f9fdd5d1a33241358cd5db11de3d8f7de0
Pyshicon <pyshicon..game@gmail.com>
2020-07-20 13:28:07 +0700

    Revised Classes.java(added icon)

--- src/main/java/io/github/pyshicon/classesreborn/classes/Classes.java
+++ src/main/java/io/github/pyshicon/classesreborn/classes/Classes.java
@@ -3,18 +3,21 @@ package io.github.pyshicon.classesreborn.classes;
 import io.github.pyshicon.classesreborn.classes.attributes.type.Constitution;
 import io.github.pyshicon.classesreborn.classes.attributes.type.Dexterity;
 import io.github.pyshicon.classesreborn.classes.attributes.type.Strength;
+import javafx.scene.paint.Material;
 
 public class Classes {
 
     private final String id;
     private final String name;
+    private final Material icon;
     private final Strength strength;
     private final Constitution constitution;
     private final Dexterity dexterity;
 
-    public Classes(String id, String name, Strength strength, Constitution constitution, Dexterity dexterity) {
+    public Classes(String id, String name, Material icon, Strength strength, Constitution constitution, Dexterity dexterity) {
         this.id = id;
         this.name = name;
+        this.icon = icon;
         this.strength = strength;
         this.constitution = constitution;
         this.dexterity = dexterity;
@@ -26,6 +29,10 @@ public class Classes {
 
     public String getName() {
         return name;
+    }
+
+    public Material getIcon() {
+        return icon;
     }
 
     public Strength getStrength() {
