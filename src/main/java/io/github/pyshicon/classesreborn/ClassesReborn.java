d81133e4d95d73a01bd88e8c33789af6cc66fcc8
Pyshicon <pyshicon..game@gmail.com>
2020-07-20 12:34:11 +0700

    Initial commit

---
+++ src/main/java/io/github/pyshicon/classesreborn/ClassesReborn.java
@@ -0,0 +1,12 @@
+package io.github.pyshicon.classesreborn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ClassesReborn extends JavaPlugin {
+
+    public void onEnable() {
+    }
+
+    public void onDisable() {
+    }
+}
